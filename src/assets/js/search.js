dd9d0e28693868fafdd8d93bd2bc673a8f04ef2d
malekannabi <malekannabi5@gmail.com>
2021-07-06 10:59:33 +0200

    added a fadein animation for the search bar on top

--- src/assets/js/search.js
+++ src/assets/js/search.js
@@ -4,7 +4,7 @@ console.log(search)
 var myScrollFunc = function() {
   var y = window.scrollY;
   if (y >= 1500) {
-    search.className = "show"
+    search.className = "show fade-in"
   } else {
     search.className = "hide"
   }
