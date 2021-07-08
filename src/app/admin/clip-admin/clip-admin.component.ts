4934f069c496b81a2970e30fae144ac54118ae15
malekannabi <malekannabi5@gmail.com>
2021-07-08 22:30:47 +0200

    reformat some code and tittle changes

--- src/app/admin/clip-admin/clip-admin.component.ts
+++ src/app/admin/clip-admin/clip-admin.component.ts
@@ -2,49 +2,71 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ClipService } from './../../services/clip.service';
 import { Component, OnInit } from '@angular/core';
-import { NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Clip } from 'src/app/models/clip';
 @Component({
   selector: 'app-clip-admin',
   templateUrl: './clip-admin.component.html',
-  styleUrls: ['./clip-admin.component.css']
+  styleUrls: ['./clip-admin.component.css'],
 })
 export class ClipAdminComponent implements OnInit {
-  isSubmitted = false
+  isSubmitted = false;
   addClip: FormGroup;
   clip: Clip = new Clip();
-  constructor(private clipService:ClipService,
+  constructor(
+    private clipService: ClipService,
     public fb: FormBuilder,
-    private router:Router,
-    public activeModal:NgbActiveModal) {
-
-      this.addClip = this.fb.group({
-      name: ['', {validators: [ Validators.required,Validators.minLength(4)],updateOn:"blur" }],
-      description:  ['', {validators: [ Validators.required,Validators.minLength(4)],updateOn:"blur" }],
-      link:  ['', {validators: [ Validators.required,Validators.minLength(4),Validators.pattern('\bclips.twitch.tv\b')],updateOn:"blur" }],
-    })
+    private router: Router,
+    public activeModal: NgbActiveModal
+  ) {
+    this.addClip = this.fb.group({
+      name: [
+        '',
+        {
+          validators: [Validators.required, Validators.minLength(4)],
+          updateOn: 'blur',
+        },
+      ],
+      description: [
+        '',
+        {
+          validators: [Validators.required, Validators.minLength(4)],
+          updateOn: 'blur',
+        },
+      ],
+      link: [
+        '',
+        {
+          validators: [
+            Validators.required,
+            Validators.minLength(4),
+            Validators.pattern('\bclips.twitch.tv\b'),
+          ],
+          updateOn: 'blur',
+        },
+      ],
+    });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   //submit add clip
-  onAddSubmit(){
+  onAddSubmit() {
     //if not valid
-    this.isSubmitted = true
-    if(this.addClip.invalid)
-       return;
-       //sending clip
-      this.clipService.createClip(this.addClip.value).subscribe((data:any) => {
-      this.clip = data;
-
+    this.isSubmitted = true;
+    if (this.addClip.invalid) return;
+    //sending clip
+    this.clipService.createClip(this.addClip.value).subscribe(
+      (data: any) => {
+        this.clip = data;
         window.location.reload();
 
         //server error
-      }, error => console.log(error));
-
-    }
-    //gettting controls
-    get getControl(){
+      },
+      (error) => console.log(error)
+    );
+  }
+  //gettting controls
+  get getControl() {
     return this.addClip.controls;
   }
 }
