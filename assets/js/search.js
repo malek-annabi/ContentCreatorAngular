b6453ca762c5dd24825dd60475ad5afe03a28704
malekannabi <malekannabi5@gmail.com>
2021-07-12 19:27:03 +0200

    Auto-generated commit [ci skip]

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,7 +2,7 @@ search = document.getElementById("scroll");
 var clips=document.getElementById('clips')
 var myScrollFunc = function() {
   var y = window.scrollY;
-  if (y >= clips.offsetTop-200) {
+  if (y >= clips.offsetTop-50) {
     search.className = "show fade-in"
   } else {
     search.className = "hide"
