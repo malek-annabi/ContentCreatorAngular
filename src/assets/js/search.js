45ca81e57f9dccc48f6af49f4d37993d7f42797f
malekannabi <malekannabi5@gmail.com>
2021-07-13 12:13:13 +0200

    search js fixed twitter widget added

--- src/assets/js/search.js
+++ src/assets/js/search.js
@@ -1,12 +1,10 @@
 search = document.getElementById("scroll");
 var clips=document.getElementById('clips')
 var myScrollFunc = function() {
-  var y = window.scrollY;
-  if (y >= clips.offsetTop-50) {
-    search.className = "show fade-in"
-  } else {
-    search.className = "hide"
-  }
+  var y = window.scrollY
+  if (y >= 1200) {
+    search.classList.add("show").add('fade-in');
+  }else{
+    search.classList.remove('show').remove('fade-in') }
 };
-
 window.addEventListener("scroll", myScrollFunc);
